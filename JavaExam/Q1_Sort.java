55d12bb792987af492f647b855e2ebe568f2d657
sreasgop <sreasgop@gmail.com>
2025-01-12 20:28:32 +0530

    Question 1 added

---
+++ JavaExam/Q1_Sort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Q1_Sort {
+
+    public static void sort(int[] arr) {
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printSortedArray(int[] arr) {
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sort(arr);
+        printSortedArray(arr);
+
+        sc.close();
+
+    }
+}
